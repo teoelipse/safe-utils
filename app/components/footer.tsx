4e152d29c48f6bbd4db668db85b0b1c4237b33fe
Josep Chetrit <jchetrit1999@gmail.com>
2025-02-26 18:58:05 +0100

    change footer and favicon

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,46 +1,41 @@
 import React from "react";
-import { Separator } from "@/components/ui/separator";
-import { Code2, Building2, ExternalLink, Heart } from "lucide-react";
+import { Heart } from "lucide-react";
 
 export function Footer() {
   return (
-    <footer className="py-0 md:px-8 md:py-0">
-      <div className="container flex flex-col items-center justify-between gap-4 md:h-12 md:flex-row">
-        <p className="text-center text-sm leading-loose text-muted-foreground md:text-left p-5 flex items-center gap-2 flex-wrap justify-center md:justify-start">
-          Developed with <Heart className="h-4 w-4 fill-current text-red-500" /> by{" "}
+    <footer className="py-4 w-full flex items-center justify-center">
+      <div className="container flex items-center justify-center">
+        <p className="text-center text-sm text-muted-foreground flex items-center gap-2 flex-wrap justify-center">
+          Developed with <Heart className="h-4 w-4 fill-current " /> by{" "}
           <a
             href="https://www.openzeppelin.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="font-medium underline underline-offset-4 inline-flex items-center gap-1"
+            className="font-medium underline underline-offset-4"
           >
             OpenZeppelin
-            <ExternalLink className="h-3 w-3" />
           </a>
           . Subject to our{" "}
           <a
             href="https://www.openzeppelin.com/tos"
             target="_blank"
             rel="noopener noreferrer"
-            className="font-medium underline underline-offset-4 inline-flex items-center gap-1"
+            className="font-medium underline underline-offset-4"
           >
-          Terms of Service
+            Terms of Service
           </a>
-          
-          . <Code2 className="h-4 w-4" /> Script by{" "}
+          . Script by{" "}
           <a
             href="https://github.com/pcaversaccio"
             target="_blank"
             rel="noopener noreferrer"
-            className="font-medium underline underline-offset-4 inline-flex items-center gap-1"
+            className="font-medium underline underline-offset-4"
           >
             pcaversaccio
-            <ExternalLink className="h-3 w-3" />
           </a>
           .
         </p>
       </div>
-      <Separator className="mt-6" />
     </footer>
   );
 }
